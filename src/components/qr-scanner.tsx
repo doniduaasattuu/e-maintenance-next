c651c783e3248c3e19c4413b2c7fa75c56aa7429
Doni Darmawan <doni.duaasattuu@gmail.com>
2025-04-04 20:21:28 +0700

    fix: error scanning and scanner rendered twice

--- src/components/qr-scanner.tsx
+++ src/components/qr-scanner.tsx
@@ -1,83 +1,126 @@
 "use client";
 
-import React from "react";
+import React, { useRef, useState } from "react"; // Import useRef
 import { Html5QrcodeScanner } from "html5-qrcode";
 import { useRouter } from "next/navigation";
 import z from "zod";
-import { toast } from "sonner";
 import { isEquipmentExist } from "@/actions/equipment-action";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { AlertCircle } from "lucide-react";
 
 export default function QRScanner() {
   const router = useRouter();
+  const qrCodeId = "qr-reader";
+  const formatEquipment = z
+    .string()
+    .length(9, { message: "Code must be 9 characters" })
+    .regex(/^[A-Z]{3}\d{6}$/, {
+      message: "Format code is invalid. Example: EMO123456",
+    });
+
+  const scannerRef = useRef<Html5QrcodeScanner | null>(null); // Gunakan useRef
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [errorHeader, setErrorHeader] = useState<string | null>(null);
 
   React.useEffect(() => {
-    // Initialize the scanner
-    const html5QrCodeScanner = new Html5QrcodeScanner(
-      "qr-reader",
-      {
-        fps: 10, // Frames per second for the scanner
-        qrbox: { width: 300, height: 300 }, // QR scanning box dimensions
-      },
-      false
-    );
+    // Hanya inisialisasi scanner jika belum ada
+    if (!scannerRef.current) {
+      scannerRef.current = new Html5QrcodeScanner(
+        "qr-reader",
+        {
+          fps: 10,
+          qrbox: { width: 300, height: 300 },
+          useBarCodeDetectorIfSupported: true,
+        },
+        false
+      );
 
-    function onScanSuccess(decodedText: string) {
-      //   router.push(route("equipments.show", decodedText.substr(0, 9)));
-      const formatEquipment = z
-        .string()
-        .length(9, { message: "Code must be 9 characters" })
-        .regex(/^[A-Z]{3}\d{6}$/, {
-          message: "Format code is invalid. Example: EMO000123",
-        });
+      function onScanSuccess(decodedText: string) {
+        const result = formatEquipment.safeParse(decodedText);
 
-      const result = formatEquipment.safeParse(decodedText);
+        if (result.success) {
+          const equipment: string = result.data;
+          const response = isEquipmentExist(equipment);
 
-      if (result.success) {
-        const equipment: string = result.data;
-        const response = isEquipmentExist(equipment);
+          setErrorHeader(null);
+          setErrorMessage(null);
 
-        response.then((status) => {
-          if (status.success) {
-            router.push(`/equipments/${equipment}`);
-            html5QrCodeScanner.clear();
-          } else {
-            html5QrCodeScanner.pause();
-            toast.error("Error", { description: status.message });
+          response.then((status) => {
+            if (status.success) {
+              if (scannerRef.current) {
+                scannerRef.current.clear().then(() => {
+                  router.push(`/equipments/${equipment}`);
+                });
+              }
+            } else {
+              if (scannerRef.current) {
+                scannerRef.current.pause();
+                // toast.error("QR Error", { description: status.message });
+                // errorHeader.current = "QR Error";
+                // errorMessage.current = status.message;
+
+                setErrorHeader("QR Error");
+                setErrorMessage(status.message);
+                setTimeout(() => {
+                  if (scannerRef.current) {
+                    scannerRef.current.resume();
+                  }
+                }, 1000);
+              }
+            }
+          });
+        } else {
+          const errorValidationMessage: string =
+            result.error.flatten().formErrors[0];
+          // toast.error("Validation Error", {
+          //   description: errorValidationMessage,
+          // });
+          setErrorHeader("Validation Error");
+          setErrorMessage(errorValidationMessage);
+
+          if (scannerRef.current) {
+            scannerRef.current.pause();
             setTimeout(() => {
-              html5QrCodeScanner.resume();
+              if (scannerRef.current) {
+                scannerRef.current.resume();
+              }
             }, 1000);
           }
-        });
-      } else {
-        const errorMessage: string = result.error.flatten().formErrors[0];
-        toast.error("Error", { description: errorMessage });
-        html5QrCodeScanner.pause();
-
-        setTimeout(() => {
-          html5QrCodeScanner.resume();
-        }, 1000);
+        }
       }
 
-      html5QrCodeScanner.clear();
+      function onScanError(decodedText: string): void {
+        console.log(decodedText);
+      }
+
+      if (scannerRef.current !== null) {
+        scannerRef.current.render(onScanSuccess, onScanError);
+      }
     }
 
-    function onScanError(decodedText: string): void {
-      toast("Error", {
-        description: `Equipment with ID: ${decodedText} is not found`,
-      });
-      html5QrCodeScanner.clear();
-    }
-
-    html5QrCodeScanner.render(onScanSuccess, onScanError);
-  }, [router]);
+    // Bersihkan scanner saat komponen unmount
+    // return () => {
+    //   if (scannerRef.current) {
+    //     console.log(`Fifth: ${scannerRef.current}`); // NOT PRINTED
+    //     scannerRef.current.clear();
+    //   }
+    // };
+  }, [formatEquipment, router]);
 
   return (
     <div className="space-y-4 mx-3 sm:mx-auto">
       <h1 className="font-semibold text-lg">Equipment QR Code Scanner</h1>
       <div
         className="sm:w-[300px] h-[270px] text-center shadow border"
-        id="qr-reader"
+        id={qrCodeId}
       />
+      {errorHeader && errorMessage && (
+        <Alert variant="destructive" className="max-w-[300px]">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>{errorHeader}</AlertTitle>
+          <AlertDescription>{errorMessage}</AlertDescription>
+        </Alert>
+      )}
     </div>
   );
 }
